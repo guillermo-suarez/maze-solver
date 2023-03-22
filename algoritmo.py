16b3094bbe0b86eeafe2e7caf316301b978f3845
Guillermo <gguillermosuarezz@gmail.com>
2023-03-22 11:55:57 -0300

    Probando Matplotlib

--- algoritmo.py
+++ algoritmo.py
@@ -1,6 +1,22 @@
+import tkinter
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 import genlab
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
 
 from genlab import printMaze, maze
 
-printMaze(maze)
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
 
+#printMaze(maze)
