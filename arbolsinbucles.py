bff34343e9d74e7bf1a605a6d48a9abb95e77008
Guillermo Suarez <gsuarez@Aspire5>
2023-03-27 14:58:45 -0300

    Cambiar de (Y, X) ---> (X, Y)

--- arbolsinbucles.py
+++ arbolsinbucles.py
@@ -4,7 +4,7 @@ from genlab import maze
 from anytree import Node, RenderTree, find_by_attr
 from colorama import Fore
 
-arbol = Node(name = "(10, 10)", cordY = 10, cordX = 10)
+arbol = Node(name = "(10, 10)", cordX = 10, cordY = 10)
 
 print(Fore.WHITE)
 
@@ -17,24 +17,25 @@ for j in range(10, 0, -1):
     for i in range (10, 0, -1):
         padre = None
         if maze[j-1][i-1] == '0':
+            nodo = Node(name = "(" + str(i) + ", " + str(j) + ")", cordY = j, cordX = i)
             # Si existe un nodo a la DERECHA para que sea su padre...
-            if find_by_attr(arbol, "(" + str(j) + ", " + str(i + 1) + ")") != None:
-                padre = find_by_attr(arbol, "(" + str(j) + ", " + str(i + 1) + ")")
+            if find_by_attr(arbol, "(" + str(i + 1) + ", " + str(j) + ")") != None:
+                padre = find_by_attr(arbol, "(" + str(i + 1) + ", " + str(j) + ")")
             # Si existe un nodo ABAJO para que sea su padre...
-            elif find_by_attr(arbol, "(" + str(j + 1) + ", " + str(i) + ")") != None:
-                padre = find_by_attr(arbol, "(" + str(j + 1) + ", " + str(i) + ")")
+            elif find_by_attr(arbol, "(" + str(i) + ", " + str(j + 1) + ")") != None:
+                padre = find_by_attr(arbol, "(" + str(i) + ", " + str(j + 1) + ")")
             # Si existe un nodo a la IZQUIERDA para que sea su padre...
-            elif find_by_attr(arbol, "(" + str(j) + ", " + str(i - 1) + ")") != None:
-                padre = find_by_attr(arbol, "(" + str(j) + ", " + str(i - 1) + ")")
+            elif find_by_attr(arbol, "(" + str(i - 1) + ", " + str(j) + ")") != None:
+                padre = find_by_attr(arbol, "(" + str(i - 1) + ", " + str(j) + ")")
             # Si existe un nodo ARRIBA para que sea su padre...
-            elif find_by_attr(arbol, "(" + str(j - 1) + ", " + str(i) + ")") != None:
-                padre = find_by_attr(arbol, "(" + str(j - 1) + ", " + str(i) + ")")
+            elif find_by_attr(arbol, "(" + str(i) + ", " + str(j - 1) + ")") != None:
+                padre = find_by_attr(arbol, "(" + str(i) + ", " + str(j - 1) + ")")
             # Si se le pudo encontrar un padre, se lo agrega al árbol
             if padre != None:
-                nodo = Node(name = "(" + str(j) + ", " + str(i) + ")", parent = padre, cordY = j, cordX = i)
+                nodo.parent = padre
             # Si su nodo padre todavía no se agregó al árbol... ¿?¿?¿?¿?
             else:
-                rel.append(Node(name = "(" + str(j) + ", " + str(i) + ")", cordY = j, cordX = i))
+                rel.append(nodo)
 
 # print(RenderTree(arbol))
 
