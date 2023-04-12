22093adb0047cbf6e4c710b2813d736f9573d2fc
Guillermo <gguillermosuarezz@gmail.com>
2023-04-12 10:18:49 -0300

    Terminado, ahora el laberinto base (variable maze) se imprime desde main.py, no desde genlab.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pa import recorrerLabPA
 from funciones import imprimirArbol, crearMatrizRecorrida, imprimirMatriz
 from colorama import Fore
 
+print("\n" + Fore.WHITE + "LABERINTO A RECORRER:\n")
+imprimirMatriz(maze)
+
 arbolPP = crearNodo(9, 9, 'I', 1)
 terminoPP = recorrerLabPP(arbolPP, arbolPP, maze)
 print(Fore.WHITE + "PRIMERO EN PROFUNDIDAD:\n")
