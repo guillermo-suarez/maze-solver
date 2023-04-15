b6b4fc5d321938cb4723bc656a86bac16b055166
srebata <lmehowicz@gmail.com>
2023-04-15 11:43:05 -0300

    Algoritmo sin hacer uso del atributo leaves

--- pa.py
+++ pa.py
@@ -1,65 +1,52 @@
 from funciones import crearNodo, existeNodo
 from anytree import Node, LevelOrderIter
 
+def hijoNodoactual(laberinto: int, y: int, x: int, nodoActual: Node, arbolPA: Node, nodosHojas: Node):
+    #Realiza todos los pasos de agregar un nodo al árbol y a la lista cuando se busca en las cuatro direcciones
+    nivel = nodoActual.level + 1
+    termino = False
+    celda = laberinto[y][x]
+    if (celda == '0' or celda == 'I') and existeNodo(arbolPA, x, y):
+        celda = 'B'
+    nuevoNodo = crearNodo(x, y, celda, nivel)
+    nuevoNodo.parent = nodoActual
+    agregoNodo = True
+    match nuevoNodo.est:
+        case 'F':
+            termino = True
+        case '0':
+            nodosHojas.append(nuevoNodo)
+    return agregoNodo, termino
+
 def recorrerLabPA(arbolPA: Node, laberinto: int):
-    #Función que permite recorrer el laberinto mediante la técnica de primero en amplitud
+    #Permite recorrer el laberinto mediante la técnica de primero en amplitud
     termino = False
     agregoNodo = True #Bandera que permite controlar si se agregó por lo menos un nodo. Por si no hay salida del laberinto
     x = 0
     y = 0
-    nivel = 1
-    raiz = arbolPA
-    nodoActual = arbolPA
+    nodosHojas = [] #Lista que solamente incluye los nodos hojas que se vayan agregando
+    nodosHojas.append(arbolPA)
 
     while (not termino and agregoNodo):
         agregoNodo = False
-        nivel = nivel + 1
-        for nodoActual in raiz.leaves:  # se itera únicamente sobre las hojas del árbol
-            if nodoActual.est == '0' or nodoActual.est == 'I':   # Se agregan hijos únicamente a los nodos hojas
-                if (nodoActual.cordY > 0 and not termino):    # Recorrer arriba
-                    x = nodoActual.cordX
-                    y = nodoActual.cordY - 1
-                    celda = laberinto[y][x]
-                    if (celda == '0' or celda == 'I') and existeNodo(arbolPA, x, y):
-                        celda = 'B'
-                    nuevoNodo = crearNodo(x, y, celda, nivel)
-                    nuevoNodo.parent = nodoActual
-                    agregoNodo = True
-                    if nuevoNodo.est == 'F':
-                        termino = True        
-                if (nodoActual.cordX > 0 and not termino):    # Recorrer izquierda
-                    x = nodoActual.cordX - 1
-                    y = nodoActual.cordY
-                    celda = laberinto[y][x]
-                    if (celda == '0' or celda == 'I') and existeNodo(arbolPA, x, y):
-                        celda = 'B'
-                    nuevoNodo = crearNodo(x, y, celda, nivel)
-                    nuevoNodo.parent = nodoActual
-                    agregoNodo = True
-                    if nuevoNodo.est == 'F':
-                        termino = True
-                if (nodoActual.cordY < 9 and not termino):    # Recorrer abajo
-                    x = nodoActual.cordX
-                    y = nodoActual.cordY + 1
-                    celda = laberinto[y][x]
-                    if (celda == '0' or celda == 'I') and existeNodo(arbolPA, x, y):
-                        celda = 'B'
-                    nuevoNodo = crearNodo(x, y, celda, nivel)
-                    nuevoNodo.parent = nodoActual
-                    agregoNodo = True
-                    if nuevoNodo.est == 'F':
-                        termino = True
-                if (nodoActual.cordX < 9 and not termino):    # Recorrer derecha
-                    x = nodoActual.cordX + 1
-                    y = nodoActual.cordY
-                    celda = laberinto[y][x]
-                    if (celda == '0' or celda == 'I') and existeNodo(arbolPA, x, y):
-                        celda = 'B'
-                    nuevoNodo = crearNodo(x, y, celda, nivel)
-                    nuevoNodo.parent = nodoActual
-                    agregoNodo = True
-                    if nuevoNodo.est == 'F':
-                        termino = True
-            '''if nodoActual.est == 'F':
-                termino = True'''
+        for nodoActual in LevelOrderIter(arbolPA):  #Se recorre el árbol por nodos del mismo nivel
+            for nodo in nodosHojas:
+                if nodoActual == nodo:
+                    if (nodoActual.cordY > 0 and not termino):    # Recorrer arriba
+                        x = nodoActual.cordX
+                        y = nodoActual.cordY - 1
+                        agregoNodo,termino = hijoNodoactual(laberinto,y,x,nodoActual,arbolPA,nodosHojas)
+                    if (nodoActual.cordX > 0 and not termino):    # Recorrer izquierda
+                        x = nodoActual.cordX - 1
+                        y = nodoActual.cordY
+                        agregoNodo,termino = hijoNodoactual(laberinto,y,x,nodoActual,arbolPA,nodosHojas)
+                    if (nodoActual.cordY < 9 and not termino):    # Recorrer abajo
+                        x = nodoActual.cordX
+                        y = nodoActual.cordY + 1
+                        agregoNodo,termino = hijoNodoactual(laberinto,y,x,nodoActual,arbolPA,nodosHojas)
+                    if (nodoActual.cordX < 9 and not termino):    # Recorrer derecha
+                        x = nodoActual.cordX + 1
+                        y = nodoActual.cordY
+                        agregoNodo,termino = hijoNodoactual(laberinto,y,x,nodoActual,arbolPA,nodosHojas)
+                    nodosHojas.remove(nodoActual)   #Se quita de la lista aquel nodo con el que ya se haya trabajado
     return termino
