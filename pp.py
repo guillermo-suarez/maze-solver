669ce9d13e48a9a520a9ea0dd98ace591bca8dfe
guillermo-suarez <gguillermosuarezz@gmail.com>
2023-04-22 17:47:26 -0300

    Terminado. Agregado clase Estado, funciones de lista y funciones de crear el arbol de expansión.

--- pp.py
+++ pp.py
@@ -1,70 +1,59 @@
-from funciones import crearNodo, existeNodo
-from anytree import Node
+from funciones import existeEstadoEnLista, Estado
 
-def recorrerLabPP(arbolPP: Node, nodoActual: Node, laberinto: int):
-    termino = False
+def busquedaPP(laberinto: int):
+    inicio = Estado(9, 9, 'I', 1, None)
+    listaVisitados = []
+    listaPendientes = []
+
+    listaPendientes.append(inicio)
+    indice = 0
     
-    if not termino:
-        # Recorrer arriba
-        if nodoActual.cordY > 0:
-            x = nodoActual.cordX
-            y = nodoActual.cordY - 1
-            celda = laberinto[y][x]
-            nivel = nodoActual.level + 1
-            if (celda == '0' or celda == 'I') and existeNodo(arbolPP, x, y):
-                celda = 'B'
-            nuevoNodo = crearNodo(x, y, celda, nivel)
-            nuevoNodo.parent = nodoActual
-            if nuevoNodo.est == '0':
-                termino = recorrerLabPP(arbolPP, nuevoNodo, laberinto)
-            elif nuevoNodo.est == 'F':
-                termino = True
-    
-    if not termino:
-        # Recorrer izquierda
-        if nodoActual.cordX > 0:
-            x = nodoActual.cordX - 1
-            y = nodoActual.cordY
-            celda = laberinto[y][x]
-            nivel = nodoActual.level + 1
-            if (celda == '0' or celda == 'I') and existeNodo(arbolPP, x, y):
-                celda = 'B'
-            nuevoNodo = crearNodo(x, y, celda, nivel)
-            nuevoNodo.parent = nodoActual
-            if nuevoNodo.est == '0':
-                termino = recorrerLabPP(arbolPP, nuevoNodo, laberinto)
-            elif nuevoNodo.est == 'F':
-                termino = True
-    
-    if not termino:
-        # Recorrer abajo
-        if nodoActual.cordY < 9:
-            x = nodoActual.cordX
-            y = nodoActual.cordY + 1
-            celda = laberinto[y][x]
-            nivel = nodoActual.level + 1
-            if (celda == '0' or celda == 'I') and existeNodo(arbolPP, x, y):
-                celda = 'B'
-            nuevoNodo = crearNodo(x, y, celda, nivel)
-            nuevoNodo.parent = nodoActual
-            if nuevoNodo.est == '0':
-                termino = recorrerLabPP(arbolPP, nuevoNodo, laberinto)
-            elif nuevoNodo.est == 'F':
-                termino = True
-    
-    if not termino:
-        # Recorrer derecha
-        if nodoActual.cordX < 9:
-            x = nodoActual.cordX + 1
-            y = nodoActual.cordY
-            celda = laberinto[y][x]
-            nivel = nodoActual.level + 1
-            if (celda == '0' or celda == 'I') and existeNodo(arbolPP, x, y):
-                celda = 'B'
-            nuevoNodo = crearNodo(x, y, celda, nivel)
-            nuevoNodo.parent = nodoActual
-            if nuevoNodo.est == '0':
-                termino = recorrerLabPP(arbolPP, nuevoNodo, laberinto)
-            elif nuevoNodo.est == 'F':
-                termino = True
-    return termino
+    while(listaPendientes and listaPendientes[0].estado != 'F'):
+        estadoActual = listaPendientes.pop(0)
+
+        # Se lo agrega a la lista de estados ya visitados
+        listaVisitados.append(estadoActual)
+
+        if estadoActual.estado != 'X':
+            # Visitar celda arriba
+            if estadoActual.y > 0:
+                x = estadoActual.x
+                y = estadoActual.y - 1
+                if not existeEstadoEnLista(listaPendientes, x, y) and not existeEstadoEnLista(listaVisitados, x, y):
+                    celda = laberinto[y][x]
+                    nuevoEstado = Estado(x, y, celda, estadoActual.nivel + 1, estadoActual)
+                    listaPendientes.insert(indice, nuevoEstado)
+                    indice = indice + 1
+            
+            # Visitar celda izquierda
+            if estadoActual.x > 0:
+                x = estadoActual.x - 1
+                y = estadoActual.y
+                if not existeEstadoEnLista(listaPendientes, x, y) and not existeEstadoEnLista(listaVisitados, x, y):
+                    celda = laberinto[y][x]
+                    nuevoEstado = Estado(x, y, celda, estadoActual.nivel + 1, estadoActual)
+                    listaPendientes.insert(indice, nuevoEstado)
+                    indice = indice + 1
+
+            # Visitar celda abajo
+            if estadoActual.y < 9:
+                x = estadoActual.x
+                y = estadoActual.y + 1
+                if not existeEstadoEnLista(listaPendientes, x, y) and not existeEstadoEnLista(listaVisitados, x, y):
+                    celda = laberinto[y][x]
+                    nuevoEstado = Estado(x, y, celda, estadoActual.nivel + 1, estadoActual)
+                    listaPendientes.insert(indice, nuevoEstado)
+                    indice = indice + 1
+
+            # Visitar celda derecha
+            if estadoActual.x < 9:
+                x = estadoActual.x + 1
+                y = estadoActual.y
+                if not existeEstadoEnLista(listaPendientes, x, y) and not existeEstadoEnLista(listaVisitados, x, y):
+                    celda = laberinto[y][x]
+                    nuevoEstado = Estado(x, y, celda, estadoActual.nivel + 1, estadoActual)
+                    listaPendientes.insert(indice, nuevoEstado)
+                    indice = indice + 1
+        
+        indice = 0
+    return listaVisitados, listaPendientes
