10cc07a5122c455d4c48ae79500963da23a5c539
Guillermo <gguillermosuarezz@gmail.com>
2023-04-26 09:52:25 -0300

    Terminado. Una ventana por arbol (PNG)

--- main.py
+++ main.py
@@ -69,13 +69,21 @@ arbolAPng(arbolPP, "expPP.png")
 arbolAPng(arbolPA, "expPA.png")
 
 sg.theme("DarkAmber")
-img = [[sg.Image(source = "expPP.png")]]
-miLayout = [[sg.Column(img, size = (1440, 810), scrollable = True)]]
-ventana = sg.Window("Title", miLayout)
+
+img1 = [[sg.Image(source = "expPP.png")]]
+layout1 = [[sg.Column(img1, size = (1920, 1080), scrollable = True)]]
+ventana1 = sg.Window("Árbol PP", layout1, finalize = True)
+
+img2 = [[sg.Image(source = "expPA.png")]]
+layout2 = [[sg.Column(img2, size = (1920, 1080), scrollable = True)]]
+ventana2 = sg.Window("Árbol PA", layout2, finalize = True)
 
 while True:
-    event, values = ventana.read()
-    if event == sg.WIN_CLOSED:
+    ventana, evento, valores = sg.read_all_windows()
+    if ventana == sg.WIN_CLOSED:
         break
+    if evento == sg.WIN_CLOSED:
+        ventana.close()
 
-ventana.close()
+ventana1.close()
+ventana2.close()
