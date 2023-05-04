4b493ae230142578ca7d833a528d7618f3a9c279
gsuarez <gsuarez@htcmisiones.gob.ar>
2023-05-04 11:57:11 -0300

    Avance

--- main.py
+++ main.py
@@ -76,8 +76,7 @@ sg.set_options(window_location = (0,0))
 
 img1 = [[sg.Image(source = "expPP.png")]]
 layout1 = [[sg.Text("HOLA", background_color = "white")], [sg.Column(img1, scrollable = True)]]
-ventana1 = sg.Window("Árbol de expansión PP", layout1, size = (int(0.9 * 1920), int(0.9 * 1080)), margins = (0, 0), finalize = True)
-ventana1.maximize()
+ventana1 = sg.Window("Árbol de expansión PP", layout1, size = (int(1920), int(1080)), margins = (0, 0), disable_minimize = True, finalize = True)
 
 # img2 = [[sg.Image(source = "expPA.png")]]
 # layout2 = [[sg.Column(img2, size = (1920, 1080), scrollable = True)]]
