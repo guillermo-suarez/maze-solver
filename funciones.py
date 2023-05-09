f4e3cd56884f8b1f11d8bb96371741b58a193166
Guillermo Suárez <gguillermosuarezz@gmail.com>
2023-05-09 11:00:29 -0300

    Avance.

--- funciones.py
+++ funciones.py
@@ -154,27 +154,27 @@ def arbolAPng(arbol: Node, path: str):
     config = []
     config.append("bgcolor = transparent")
     config.append("edge[color = white]")
-    config.append('''
+    config.append('''subgraph REF
     {
-        mindist=0;
-        ranksep=0;
-        nodesep=0;
+        label = "Referencias";
 
         node[shape = box, margin="0,0", width = 1, height = 1, color = white, fontcolor = white];
         edge[style=invis];
 
-        Legend[width=2];
-        Legend -> Foo;
-        Legend -> FooValue;
-        Foo -> Bar;
-        FooValue -> BarValue
-        Bar -> Baz;
-        BarValue -> BazValue;
+        Verde -> Rojo;
+        Rojo -> Azul;
+        Azul -> Trazos;
+
+        VerdeT -> RojoT;
+        RojoT -> AzulT;
+        AzulT -> TrazosT;
+
+        Verde -> VerdeT;
+        Rojo -> RojoT;
+        Azul -> AzulT;
+        Trazos -> TrazosT;
 
         edge [constraint=false];
-        Foo -> FooValue;
-        Bar -> BarValue
-        Baz -> BazValue;
     }''')
     arbolDot = DotExporter(arbol,
                            nodeattrfunc = lambda n: 'label = "#%s\n%s", style = %s, fillcolor = %s, color = %s, fontcolor = white, fontname = \"Arial\"'
