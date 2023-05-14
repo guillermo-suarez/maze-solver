8b0ef01d73b2984100ef4b0a5600aa2d58add079
srebata <lmehowicz@gmail.com>
2023-05-14 11:35:33 -0300

    Finalizado pruebaEspacio

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,8 @@
 from genlab import getMaze
 from pp import recorrerLabPP
 from pa import recorrerLabPA
-from funciones import crearArbolExpansion
-from anytree import Node, LevelOrderIter
+from funciones import crearArbolExpansion, imprimirArbol, imprimirMatriz
+from anytree import LevelOrderIter
 import time
 
 def pruebaTiempo():
@@ -62,4 +62,44 @@ def pruebaOptimo():
             print('La salida fue en la casilla [',nodo.cordX,'][',nodo.cordY,']')
             print('La profundidad del árbol es de',nodo.depth)
 
-pruebaOptimo()
+def pruebaEspacio():
+    filas = columnas = 10
+    rango = 10000
+    bMaxPP = bMaxPA = 0 #Factor de ramificación máximo de todos los laberintos
+    dMaxPP = dMaxPA = 0 #Profundidad de la salida del árbol con el bMax
+    encontrado = False
+
+    for i in range(rango):
+        laberinto = getMaze(filas, columnas)
+        visitadosPP, pendientesPP = recorrerLabPP(laberinto)
+        visitadosPA, pendientesPA = recorrerLabPA(laberinto)
+        arbolPP = crearArbolExpansion(visitadosPP, pendientesPP)
+        arbolPA = crearArbolExpansion(visitadosPA, pendientesPA)
+        for nodo in LevelOrderIter(arbolPP):
+            if len(nodo.children) > bMaxPP:
+                bMaxPP = len(nodo.children)
+                encontrado = True
+            if nodo.est == 'F' and encontrado:
+                dMaxPP = nodo.depth
+                encontrado = False
+        for nodo in LevelOrderIter(arbolPA):
+            if len(nodo.children) > bMaxPA:
+                bMaxPA = len(nodo.children)
+                encontrado = True
+            if nodo.est == 'F' and encontrado:
+                dMaxPA = nodo.depth
+                encontrado = False
+    print('Prueba de',rango,'laberintos')
+    print('\tPrimero en profundidad:')
+    print('\t\tbMax = ',bMaxPP)
+    print('\t\tdMax = ',dMaxPP)
+    print('\tPrimero en amplitud:')
+    print('\t\tbMax = ',bMaxPA)
+    print('\t\tdMax = ',dMaxPA)
+    if bMaxPP == bMaxPP and dMaxPP == dMaxPA:
+        print('Ambos algoritmos poseen la misma complejidad')
+    else:
+        print('La complejidad máxima de primero en profundidad es de', bMaxPP**dMaxPP)
+        print('La complejidad máxima de primero en amplitud es de', bMaxPA**dMaxPA)
+
+
